cfbb98cd5bd7c4a0719952c2be7c269a768a3957
M3gumii <144289754+M3gumii@users.noreply.github.com>
2026-02-15 17:51:51 -0500

    home main activity buttons working...

--- app/src/main/java/com/example/recyclingapp/MainActivity.kt
+++ app/src/main/java/com/example/recyclingapp/MainActivity.kt
@@ -1,22 +1,48 @@
 package com.example.recyclingapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : FragmentActivity() {
+class MainActivity : AppCompatActivity() {  //AppCompatActivity to hold fragments...
+    //Use fragmentActivity for fragments!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)  //Set the main layout via activity_main...
 
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+        /**
+         * COMPONENTS
+         */
+        var botNavView: BottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_selection_options);
+
+
+        /**
+         * LISTENERS
+         */
+
+        //Sets up the bottom button selection options to switch screens...
+        botNavView.setOnItemReselectedListener{ selection ->
+            when(selection.itemId){ //Use when (java if p much) to select what to swap to!
+                R.id.home_select_button -> {
+                    //Send to home screen
+                    System.out.println("HOME SELECTED!!")
+                }
+                R.id.search_select_button -> {
+                    //Send to search screen
+                }
+                R.id.camera_select_button -> {
+                    //send to camera scanning screen
+                }
+            }
+
+        }
+
     }
 }
