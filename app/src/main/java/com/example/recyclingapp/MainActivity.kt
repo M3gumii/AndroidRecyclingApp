96f8b7cbb0c897f241276ae55e77f9b1e52a4897
M3gumii <144289754+M3gumii@users.noreply.github.com>
2026-02-16 16:45:27 -0500

    Fragment outline created! Not yet swapping correctly...

--- app/src/main/java/com/example/recyclingapp/MainActivity.kt
+++ app/src/main/java/com/example/recyclingapp/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {  //AppCompatActivity to hold fragments...
@@ -16,6 +18,10 @@ class MainActivity : AppCompatActivity() {  //AppCompatActivity to hold fragment
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)  //Set the main layout via activity_main...
+
+        //Fragment manager to set screens!
+        val fm = supportFragmentManager;
+        var currentFragment: Fragment?;
 
         /**
          * COMPONENTS
@@ -30,15 +36,28 @@ class MainActivity : AppCompatActivity() {  //AppCompatActivity to hold fragment
         //Sets up the bottom button selection options to switch screens...
         botNavView.setOnItemReselectedListener{ selection ->
             when(selection.itemId){ //Use when (java if p much) to select what to swap to!
-                R.id.home_select_button -> {
-                    //Send to home screen
+                R.id.home_select_button -> {    //Send to home screen!
                     System.out.println("HOME SELECTED!!")
+                    currentFragment = fm.findFragmentById(R.id.home_fragment);
+                    if(currentFragment == null){
+                        currentFragment = CameraFragment();
+                        fm.beginTransaction().add(R.id.fragment_container, HomeFragment()).commit();
+                    }
                 }
                 R.id.search_select_button -> {
                     //Send to search screen
                 }
                 R.id.camera_select_button -> {
                     //send to camera scanning screen
+                    System.out.println("CAMERA SELECTED!!")
+                    currentFragment = fm.findFragmentById(R.id.camera_fragment);
+                    if(currentFragment == null){
+                        currentFragment = CameraFragment();
+                        fm.beginTransaction().add(R.id.fragment_container, CameraFragment()).commit();
+                    }
+                }
+                R.id.recent_select_button -> {
+
                 }
             }
 
