bd5d62504c578dd1fd7bd166e8548bdd20bbfcd1
M3gumii <144289754+M3gumii@users.noreply.github.com>
2026-03-01 18:26:20 -0500

    Home screen changed based on if the user is logged in or not.

--- app/src/main/java/com/example/recyclingapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/recyclingapp/fragments/HomeFragment.kt
@@ -10,11 +10,22 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.recyclingapp.MainActivity
 import com.example.recyclingapp.R
+import com.example.recyclingapp.viewmodels.UserViewModel
+import kotlin.getValue
 
 class HomeFragment : Fragment(R.layout.home_fragment) {
 
     private val mlogTag: String = "Home Fragment";
+
+    /**
+     * View model
+     */
+    private val userViewModel: UserViewModel by activityViewModels {
+        (requireActivity() as MainActivity).appViewModelFactory
+    }
 
     val columbus_recycling_url = "https://www.columbus.gov/Services/Trash-Recycling-Bulk-Collection/Recycling";
     val rumpke_recycling_url = "https://www.rumpke.com/about-us/service-areas/oh/franklin-county/columbus/recycling-resource-center";
@@ -29,6 +40,7 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         Log.d(mlogTag, "onViewCreated called!");
 
         //User info
@@ -40,9 +52,6 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
         val columbusRecyclingLink: Button = view.findViewById<Button>(R.id.columbus_recycling_button_link);
         val rumpkeRecyclingLink: Button = view.findViewById<Button>(R.id.rumpke_recycling_button_link);
 
-        //Todo - set boxes based on if the user logged in or not!
-        //TODO - Set numItemsRecycledBox based on num items recycled!
-
         columbusRecyclingLink.setOnClickListener {
             //brings up window by the link!
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(columbus_recycling_url));
@@ -52,7 +61,18 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(rumpke_recycling_url)));
         }
 
-        super.onViewCreated(view, savedInstanceState)
+        if(userViewModel.selectedUser.value != null){
+            userNameBox.text = userViewModel.selectedUser.value!!.username;
+            userInfoBox.text = "Welcome " + userViewModel.selectedUser.value!!.username + "!\nLet's recycle together!!!";
+        }
+
+        //Whenever user info changes, these should auto be updated!
+        userViewModel.selectedUser.observe(viewLifecycleOwner){ user ->
+            if(user != null){
+                numItemsRecycledBox.text = user.num_items_recycled.toString();
+            }
+        }
+
     }
 
     override fun onDestroyView(){
