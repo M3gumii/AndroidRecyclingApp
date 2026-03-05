17c98e1069575458fb170178f6b67fd8b8c18136
M3gumii <144289754+M3gumii@users.noreply.github.com>
2026-03-04 21:09:53 -0500

    Added item display for selected items...

--- app/src/main/java/com/example/recyclingapp/fragments/ItemDisplayFragment.kt
+++ app/src/main/java/com/example/recyclingapp/fragments/ItemDisplayFragment.kt
@@ -6,13 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModel
+import coil.load
+import com.example.recyclingapp.MainActivity
 import com.example.recyclingapp.R
+import com.example.recyclingapp.viewmodels.PackageViewModel
+import org.w3c.dom.Text
 
 class ItemDisplayFragment : Fragment(R.layout.item_display_layout) {
 
 
     private val mlogTag: String = "Item Display Fragment";
+
+    /**
+     * ViewModel!
+     */
+    val packageViewModel: PackageViewModel by activityViewModels {
+        (requireActivity() as MainActivity).appViewModelFactory
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,7 +43,28 @@ class ItemDisplayFragment : Fragment(R.layout.item_display_layout) {
         Log.d(mlogTag, "onViewCreated called!")
 
         val closeButton: Button = view.findViewById<Button>(R.id.closeButton);
+        var description: TextView = view.findViewById<TextView>(R.id.item_description);
+        var name: TextView = view.findViewById<TextView>(R.id.item_name)
+        var image: ImageView = view.findViewById<ImageView>(R.id.item_image)
+        var isRecyclable: TextView = view.findViewById<TextView>(R.id.item_recyclable)
 
+        packageViewModel.getPackage("036000291452")
+
+        /**
+         * If there is some item selected, then the data should be filled in!
+         */
+        packageViewModel.selectedPackage.observe(viewLifecycleOwner){
+            description.text = packageViewModel.selectedPackage.value?.description;
+            name.text = packageViewModel.selectedPackage.value!!.name;  //Always a name
+            isRecyclable.text = "Recyclable: " + packageViewModel.selectedPackage.value!!.recycling_pos.toString();
+            if(packageViewModel.selectedPackage.value?.image_link != null) {    //Load the image using coil if present...
+                image.load(packageViewModel.selectedPackage.value!!.image_link){
+                    crossfade(true)
+                    placeholder(R.drawable.recycle)
+                    error(R.drawable.recycle)
+                }
+            }
+        }
 
         /**
          * Listeners...
